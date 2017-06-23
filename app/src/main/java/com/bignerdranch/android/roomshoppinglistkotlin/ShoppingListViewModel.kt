77c30701eee8036562d0b272d708f175a0a5f0da
Rashad cureton <rashad.cureton@gmail.com>
2017-06-23 16:53:48 -0400

    added get items method to viewmodel

--- app/src/main/java/com/bignerdranch/android/roomshoppinglistkotlin/ShoppingListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/roomshoppinglistkotlin/ShoppingListViewModel.kt
@@ -2,8 +2,25 @@ package com.bignerdranch.android.roomshoppinglistkotlin
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LiveData
+import com.bignerdranch.android.roomshoppinglistkotlin.database.AppDatabase
+import com.bignerdranch.android.roomshoppinglistkotlin.database.ShoppingItems
 
+private var mDatabase: AppDatabase? = null
+private var mShoppingItems: LiveData<List<ShoppingItems>> = null!!
 
 class ShoppingListViewModel(application: Application?): AndroidViewModel(application) {
+
+    init {
+        mDatabase = AppDatabase.getDatabase(this.getApplication())
+        mShoppingItems = mDatabase!!.shoppingItemsDao().getAllItems()
+    }
+
+    companion object {
+        fun getItems(): LiveData<List<ShoppingItems>> {
+            return mShoppingItems
+        }
+    }
+
 
 }
--- app/src/main/java/com/bignerdranch/android/roomshoppinglistkotlin/database/ShoppingItems.kt
+++ app/src/main/java/com/bignerdranch/android/roomshoppinglistkotlin/database/ShoppingItems.kt
@@ -3,9 +3,11 @@ package com.bignerdranch.android.roomshoppinglistkotlin.database
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import android.arch.persistence.room.TypeConverters
 import java.util.*
 
 @Entity(tableName = "shoppingItems")
+@TypeConverters(DateConverter::class)
 data class ShoppingItems (
         @PrimaryKey(autoGenerate = true) val id: Int, @ColumnInfo(name = "date") var date: Date,
         @ColumnInfo(name = "item") var item: String, @ColumnInfo(name = "store") var store: String)
